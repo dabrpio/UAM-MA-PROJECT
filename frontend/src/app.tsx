5e8760b4f5f449a08ffa650a0366a0cbca1c0232
Piotr Wrzodak <piotrwrzodakk@gmail.com>
2024-09-18 20:47:12 +0200

    add theme mode toggle

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,1 +1,12 @@
-export const App = () => <></>;
+import { ModeToggle } from "./components/mode-toggle";
+import { ThemeProvider } from "./components/theme-provider";
+
+export const App = () => {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <>
+        <ModeToggle />
+      </>
+    </ThemeProvider>
+  );
+};
